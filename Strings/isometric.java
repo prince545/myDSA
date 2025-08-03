fd41322889bf7c8461ddcfe6cc2021bb6fd72ace
Simerjeet singh <daljeetkaur9570@gmail.com>
2025-08-03 18:38:52 +0530

    commit

---
+++ Strings/isometric.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] mapST = new int[256];  // map from s to t
+        int[] mapTS = new int[256];  // map from t to s
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (mapST[c1] == 0 && mapTS[c2] == 0) {
+                mapST[c1] = c2;
+                mapTS[c2] = c1;
+            } else {
+                if (mapST[c1] != c2 || mapTS[c2] != c1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
