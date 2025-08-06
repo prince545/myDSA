c990257976950d4adbb89b1d51dd6579b6a89751
Simerjeet singh <daljeetkaur9570@gmail.com>
2025-08-06 19:15:29 +0530

    commit

---
+++ Strings/6-8-25/Roman to integer.java
@@ -0,0 +1,14 @@
+
+public static String intToRoman(int num){
+    int [] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String [] symbols = {"M", "CM", "D", "CD", "C
+}
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < values.length && num > 0; i++) {
+        while (num >= values[i]) {
+            sb.append(symbols[i]);
+            num -= values[i];
+        }
+    }
+        return sb.toString();
+    }
